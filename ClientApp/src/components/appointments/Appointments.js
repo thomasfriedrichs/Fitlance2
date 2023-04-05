a7e7359c56674b9403e89458702624aa5b3e45a6
Thomas Friedrichs <friedrichs.tommy@gmail.com>
2023-04-05 14:10:36 -0700

    adjust-calendar-styles

--- ClientApp/src/components/appointments/Appointments.js
+++ ClientApp/src/components/appointments/Appointments.js
@@ -1,21 +1,18 @@
 ﻿import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import Cookies from "js-cookie";
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 import { getUserAppointments, getTrainerAppointments } from "../../services/AppointmentService";
 import SingleAppointment from "./SingleAppointment";
+import "./calendar-styles.css";
 
 const Appointments = () => {
     const role = Cookies.get("Role");
     const { data, isLoading, isError, error } = useQuery(["getAppointments"], role === "User" ? getUserAppointments : getTrainerAppointments);
     const localizer = momentLocalizer(moment)
-
-    const events = () => {
-
-    }
 
     if (isLoading) {
         return <div className="my-72">Loading...</div>
@@ -34,7 +31,7 @@ const Appointments = () => {
                     </h1>
                 </div>
                 <div className="md:flex md:justify-around md:items-start md:space-x-2 mt-6">
-                    <div className="h-96">
+                    <div className="h-[32rem]">
                         <Calendar
                             startAccessor="start"
                             endAccessor="end"
